b25bd2c636021a3651e49029e42ea00ce6dfa5ec
muhtomakin <muhtomakin@gmail.com>
2022-11-29 21:42:17 +0100

    adding-new-features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { CarComponent } from './components/car/car.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ColorComponent } from './components/color/color.component';
+import { RentalComponent } from './components/rental/rental.component';
+import { CardComponent } from './components/card/card.component';
+import { ImgComponent } from './components/img/img.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NaviComponent,
+    BrandComponent,
+    CarComponent,
+    CustomerComponent,
+    FooterComponent,
+    ColorComponent,
+    RentalComponent,
+    CardComponent,
+    ImgComponent
   ],
   imports: [
     BrowserModule,
